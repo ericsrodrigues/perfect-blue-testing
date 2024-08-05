3c37530a661d7312c4f79857a3fa56f196af52a6
Eric <50216183+ericsrodrigues@users.noreply.github.com>
2024-08-05 19:42:11 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,12 +12,43 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+### Third Party Repos
+
+# VSCode Repo
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+# Docker Repo
+
+tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
+[docker-ce-stable]
+name=Docker CE Stable - $basearch
+baseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install \
+  code \
+  openssl \
+  zsh \
+  neovim \
+  gnome-tweaks \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin \
+  bootc \
+  rclone
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
+systemctl start docker
 systemctl enable podman.socket
