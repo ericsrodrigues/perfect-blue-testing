8be77e5c0c416b40b213e5c346260fad252706ae
Eric <50216183+ericsrodrigues@users.noreply.github.com>
2024-10-15 09:12:52 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,8 +16,14 @@ RELEASE="$(rpm -E %fedora)"
 
 # VSCode Repo
 
-# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
 
 # Docker Repo
 
