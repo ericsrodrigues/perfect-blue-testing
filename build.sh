d757c2fee41f523e8d60bfbe47f61175bf67ce59
Eric <50216183+ericsrodrigues@users.noreply.github.com>
2024-11-14 22:35:09 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,37 +36,35 @@ gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
 EOF
 
-# SwayNC
-
-sudo curl -sL -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:erikreider:SwayNotificationCenter.repo https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/repo/fedora-$(rpm -E %fedora)/erikreider-SwayNotificationCenter-fedora-$(rpm -E %fedora).repo
-
 # this installs a package from fedora repos
 rpm-ostree install \
-  code \
-  zsh \
-  neovim \
-  vim \
-  gnome-tweaks \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin \
-  bootc \
-  rclone \
-  fastfetch \
-  jetbrains-mono-fonts \
-  google-noto-sans-cjk-fonts \
-  SwayNotificationCenter \
-  alacritty \
-  gammastep \
-  gh
+code \
+openssl \
+zsh \
+neovim \
+vim \
+gnome-tweaks \
+docker-ce \
+docker-ce-cli \
+containerd.io \
+docker-buildx-plugin \
+docker-compose-plugin \
+bootc \
+rclone \
+fastfetch \
+nautilus-gsconnect \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-gsconnect \
+gnome-shell-extension-light-style \
+gnome-shell-extension-blur-my-shell \
+jetbrains-mono-fonts \
+google-noto-sans-cjk-fonts \
+gh
 
 # uninstall packages
 rpm-ostree uninstall \
-  firefox \
-  firefox-langpacks \
-  foot
+firefox \
+firefox-langpacks
 
 #### Example for enabling a System Unit File
 
